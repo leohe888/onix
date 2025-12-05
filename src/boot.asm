c8cb0232f354ffac316fc493c14607ec3778a147
leohe <2979746577@qq.com>
2025-12-05 10:42:15 +0800

    内核加载器

--- src/boot.asm
+++ src/boot.asm
@@ -11,25 +11,20 @@ mov es, ax
 mov ss, ax
 mov sp, 0x7c00
 
+; 打印字符串
 mov si, booting
 call print
 
-xchg bx, bx; bochs 魔数断点
-
 mov edi, 0x1000; 目标地址
-mov ecx, 0; 起始扇区
-mov bl, 1; 扇区数量
+mov ecx, 2; 起始扇区
+mov bl, 4; 扇区数量
 
 call read_disk
 
-xchg bx, bx; bochs 魔数断点
+cmp word [0x1000], 0x55aa
+jnz error
 
-mov edi, 0x1000; 目标地址
-mov ecx, 2; 起始扇区
-mov bl, 1; 扇区数量
-call write_disk
-
-xchg bx, bx; bochs 魔数断点
+jmp 0:0x1002
 
 ; 阻塞
 jmp $
@@ -111,83 +106,6 @@ read_disk:
             loop .readw
         ret
 
-write_disk:
-    ; 设置读写扇区的数量
-    mov dx, 0x1f2
-    mov al, bl
-    out dx, al
-
-    ; 设置 LBA 的 0-7 位
-    inc dx; 0x1f3
-    mov al, cl
-    out dx, al
-
-    ; 设置 LBA 的 8-15 位
-    inc dx; 0x1f4
-    shr ecx, 8
-    mov al, cl
-    out dx, al
-
-    ; 设置 LBA 的 16-23 位
-    inc dx; 0x1f5
-    shr ecx, 8
-    mov al, cl
-    out dx, al
-
-    ; 设置寻址模式，驱动器，以及 LBA 的 24-27 位
-    inc dx; 0x1f6
-    shr ecx, 8
-    and cl, 0b1111; 将高四位置为 0
-    mov al, 0b1110_0000; LBA 模式，主盘
-    or al, cl
-    out dx, al
-
-    inc dx; 0x1f7
-    mov al, 0x30; 写命令
-    out dx, al
-
-    xor ecx, ecx; 将 ecx 清零
-    mov cl, bl; 将扇区数存入 cl
-
-    .write:
-        push cx; 保存 cx
-        call .writes; 写入一个扇区
-        call .waits; 等待硬盘繁忙结束
-        pop cx; 恢复 cx
-        loop .write
-
-    ret
-
-    .waits:
-        mov dx, 0x1f7
-        .check:
-            in al, dx
-
-            ; 一点点延迟
-            jmp $ + 2
-            jmp $ + 2
-            jmp $ + 2
-
-            and al, 0b1000_0000
-            cmp al, 0b0000_0000
-            jnz .check
-        ret
-    
-    .writes:
-        mov dx, 0x1f0
-        mov cx, 256; 一个扇区 256 个字
-        .writew:
-            mov ax, [edi]
-            out dx, ax
-
-            ; 一点点延迟
-            jmp $ + 2
-            jmp $ + 2
-            jmp $ + 2
-            add edi, 2
-            loop .writew
-        ret
-
 print:
     mov ah, 0x0e
 .next:
@@ -202,6 +120,13 @@ print:
 
 booting:
     db "Booting Onix...", 10, 13, 0; \n\r\0
+
+error:
+    mov si, .msg
+    call print
+    hlt
+    jmp $
+    .msg db "Booting Error!!!", 10, 13, 0; \n\r\0
 
 ; 将剩余的空间填充为 0
 times 510 - ($ - $$) db 0

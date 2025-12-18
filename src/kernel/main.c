a0b829a2a9b74c8e45a86630af2fef4ea1150b72
leohe <2979746577@qq.com>
2025-12-18 19:47:06 +0800

    基础显卡驱动

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,38 +2,19 @@
 #include <onix/types.h>
 #include <onix/io.h>
 #include <onix/string.h>
+#include <onix/console.h>
 
-// #define CRT_ADDR_REG 0x3d4
-// #define CRT_DATA_REG 0x3d5
-
-// #define CRT_CURSOR_H 0xe
-// #define CRT_CURSOR_L 0xf
-
-char message[] = "hello onix!!!";
+char message[] = "hello onix!!!\n";
 char buf[1024];
 
 void kernel_init()
 {
-    // outb(CRT_ADDR_REG, CRT_CURSOR_H);
-    // u16 pos = inb(CRT_DATA_REG) << 8;
-    // outb(CRT_ADDR_REG, CRT_CURSOR_L);
-    // pos |= inb(CRT_DATA_REG);
-
-    int res;
-    res = strcmp(buf, message);
-    strcpy(buf, message);
-    res = strcmp(buf, message);
-    strcat(buf, message);
-    res = strcmp(buf, message);
-    res = strlen(message);
-    res = sizeof(message);
-
-    char *ptr = strchr(message, '!');
-    ptr = strrchr(message, '!');
-
-    memset(buf, 0, sizeof(buf));
-    res = memcmp(buf, message, sizeof(message));
-    memcpy(buf, message, sizeof(message));
-    res = memcmp(buf, message, sizeof(message));
-    ptr = memchr(buf, '!', sizeof(message));
+    console_init();
+    
+    // u32 count = 30;
+    while (true)
+    {
+        console_write(message, sizeof(message) - 1);
+    }
+    
 }

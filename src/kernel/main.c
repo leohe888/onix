843fdcb77a9b9692d1471ebd431ea398785aa991
leohe <2979746577@qq.com>
2025-12-19 21:16:15 +0800

    调试工具

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -5,11 +5,13 @@
 #include <onix/console.h>
 #include <onix/printk.h>
 #include <onix/assert.h>
+#include <onix/debug.h>
 
 void kernel_init()
 {
     console_init();
-    assert(3 < 5);
-    // assert(3 > 5);
-    panic("Kernel panic");
+
+    BMB;
+    
+    DEBUGK("debug onix!!!\n");
 }
